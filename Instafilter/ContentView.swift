644f1489ff1b1a1382e7cc13ead9b969232df9be
Sebastien REMY <seb.remy@me.com>
2020-07-05 10:23:55 +0200

    initial commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Sebastien REMY on 05/07/2020.
+//  Copyright © 2020 MonkeyDev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var blurAmount: CGFloat = 0
+    
+    var body: some View {
+     VStack {
+            Text("Hello, World!")
+                .blur(radius: blurAmount)
+
+            Slider(value: $blurAmount, in: 0...20)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
